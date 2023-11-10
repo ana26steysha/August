4da7da3f15f9059ce3257009a3196863362d4b93
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-10 19:56:44 +0200

    add map

--- src/component/Hotels.jsx
+++ src/component/Hotels.jsx
@@ -1,59 +1,23 @@
 import React, { useState, useEffect } from "react";
+import ReactMapGl from "react-map-gl"; // Correct import statement
+import { Token } from "@mui/icons-material";
 
 function Hotels() {
-  const [data, setData] = useState(null); // Создайте состояние для хранения данных
-
-  const getData = async () => {
-    try {
-      const response = await fetch("https://dummyjson.com/carts", {
-        method: "get",
-        mode: "cors",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
-      const result = await response.json(); // Преобразуйте ответ в JSON формат
-      setData(result); // Обновите состояние данными
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    getData(); // Вызовите функцию для получения данных при загрузке компонента
-  }, []);
-
   return (
-    <div className="booton">
-      <button onClick={getData}>Загрузить данные</button>
-      <div>
-        {data && data.carts ? (
-          <div>
-            <h2>Полученные данные:</h2>
-            {data.carts.map((cart) => (
-              <div key={cart.id}>
-                <p>Total: ${cart.total}</p>
-                <p>Discounted Total: ${cart.discountedTotal}</p>
-                <p>Total Products: {cart.totalProducts}</p>
-                <p>Total Quantity: {cart.totalQuantity}</p>
-                <h4>Products in the Cart:</h4>
-                {cart.products.map((product) => (
-                  <div key={product.id}>
-                    <h5>{product.title}</h5>
-                    <p>Price: ${product.price}</p>
-                    <p>Quantity: {product.quantity}</p>
-                    <p>Total: ${product.total}</p>
-                    <p>Discounted Price: ${product.discountedPrice}</p>
-                    <img src={product.thumbnail} alt={product.title} />
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Данные еще не загружены.</p>
-        )}
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 2 }}>{/* Контент слева */}</div>
+      <div style={{ flex: 1 }}>
+        <iframe
+          width="759"
+          height="846"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?width=759&amp;height=746&amp;hl=en&amp;q=%20Timi%C8%99oara+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+        <div className="cardApi"></div>
       </div>
     </div>
   );
