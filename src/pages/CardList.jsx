da588c8421acd3739904baa843c229a8c864e5cc
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-29 07:14:51 +0200

    то что находиться в листе карточек мар

---
+++ src/pages/CardList.jsx
@@ -0,0 +1,41 @@
+export const CardList = [
+  {
+    name: "Italy",
+    title: "300",
+    image:
+      "https://www.erv.ru/upload/medialibrary/587/ww7gich3fbgmb19hqv6z82r8ithpch0x/%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D1%8B%D0%B9%20%D0%9A%D0%BE%D0%BB%D0%B8%D0%B7%D0%B5%D0%B9%20%D0%BD%D0%B0%20%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%B5%20%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0,%20%D0%A0%D0%B8%D0%BC,%20%D0%98%D1%82%D0%B0%D0%BB%D0%B8%D1%8F.jpg",
+    id: 1,
+  },
+  {
+    name: "Franse",
+    title: "300",
+    image: "https://londongroup.ru/upload/medialibrary/a72/1.jpg",
+    id: 2,
+  },
+  {
+    name: "Spain",
+    title: "300",
+    image: "https://sunny-days.by/images/countries/spane.jpg",
+    id: 3,
+  },
+  {
+    name: "Türkiye",
+    title: "300",
+    image: "https://ava.kg/wp-content/uploads/2017/12/turkey_3.jpg",
+    id: 4,
+  },
+  {
+    name: "England",
+    title: "300",
+    image:
+      "https://www.vsedostoprimechatelnosti.ru/assets/images/evropa/angliya/angliya.jpg",
+    id: 5,
+  },
+  {
+    name: "Norway",
+    title: "300",
+    image:
+      "https://visitukraine.today/media/blog/previews/L5tiPFVzBlApVSsqpOHPjKiJjDNBHhU10kZOvceJ.jpg",
+    id: 6,
+  },
+];
