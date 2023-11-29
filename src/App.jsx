4db3509a460f5440d18bca14186def7f340578ee
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-29 07:43:02 +0200

    главный сайт с роутами

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import About from "./components/About";
-import Information from "./components/Information";
-import Blog from "./components/Blog";
-import RegistrationPage from "./components/RegistrationPage"; // Импортируйте вашу форму
+import MoveBar from "./component/MoveBar";
+import SidebarData from "./component/SidebarData";
+import Home from "./pages/Home";
+import CustomSlider from "./pages/CustomSlider";
+import Comments from "./pages/Comments";
+import Information from "./pages/Information";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import Routercards from "./pages/Routercards";
+import Hotels from "./component/Hotels";
+
+import { Home as HomeIcon } from "@mui/icons-material";
 
 const App = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClicked = () => {
+    setClicked(!clicked);
+  };
   return (
     <div>
-      <ResponsiveAppBar />
+      <MoveBar clicked={clicked} isClicked={handleClicked} />
+      {clicked ? <SidebarData /> : null}
       <Routes>
-        <Route path="/about" element={<About />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/" element={<Home />} />
         <Route path="/information" element={<Information />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/" element={<RegistrationPage />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/CustomSlider" element={<CustomSlider />} />
+        <Route path="/Routercards" element={<Routercards />} />
+        <Route path="/Comments" element={<Comments />} />
       </Routes>
     </div>
   );
