5eb39afb32b295320fbd344020a8193519864af5
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-29 07:43:55 +0200

    конфиг

---
+++ src/next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-originallow-popups",
+          },
+        ],
+      },
+    ];
+  },
+};
