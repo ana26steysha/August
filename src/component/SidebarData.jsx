df8862fd142fc4a6cfab4a7f35bf132629fbcb5d
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-29 07:12:17 +0200

    навбар 2 часть с роутами

--- src/component/SidebarData.jsx
+++ src/component/SidebarData.jsx
@@ -1,46 +1,36 @@
 import React from "react";
-import { RiLoginBoxLine } from "react-icons/ri";
-import { FaRegRegistered } from "react-icons/fa";
+import "../styles/SidebarData.css";
+import { Link, NavLink } from "react-router-dom";
 
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
+function SidebarData() {
+  return (
+    <>
+      <div className="Navbars">
+        <ul className="NavbarWrappers">
+          <li className="NavbarElement">
+            <NavLink className="link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="NavbarElement">
+            <NavLink className="link" to="/about-us">
+              About Us
+            </NavLink>
+          </li>
+          <li className="NavbarElement">
+            <NavLink className="link" to="/contact-us">
+              Contact Us
+            </NavLink>
+          </li>
+          <li className="NavbarElement">
+            <NavLink className="linkbtn" to="/login">
+              login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
 
-export const SidebarData = [
-  {
-    title: "Login",
-    path: "/",
-    icon: <RiLoginBoxLine />,
-    cName: "nav-text",
-  },
-  {
-    title: "Registration",
-    path: "/registration",
-    icon: <FaRegRegistered />,
-    cName: "nav-text",
-  },
-  {
-    title: "Information",
-    path: "/information",
-    icon: <FaIcons.FaCartPlus />,
-    cName: "nav-text",
-  },
-  {
-    title: "Team",
-    path: "/team",
-    icon: <IoIcons.IoMdPeople />,
-    cName: "nav-text",
-  },
-  {
-    title: "Messages",
-    path: "/messages",
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: "nav-text",
-  },
-  {
-    title: "Support",
-    path: "/support",
-    icon: <IoIcons.IoMdHelpCircle />,
-    cName: "nav-text",
-  },
-];
+export default SidebarData;
