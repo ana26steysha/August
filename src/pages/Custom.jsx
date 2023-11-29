b542c21e64524c1016445fccd69b722c1741f341
ana26steysha <anastasiadontsu26@gmail.com>
2023-11-29 07:19:29 +0200

    масив с мар для карточек скрола

---
+++ src/pages/Custom.jsx
@@ -0,0 +1,49 @@
+export const dataDigitalBestSeller = [
+  {
+    id: 1,
+    title: "England",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://media.cnn.com/api/v1/images/stellar/prod/160418173456-beautiful-england-18-london-england.jpg?q=w_1900,h_1095,x_0,y_0,c_fill/h_618",
+  },
+  {
+    id: 2,
+    title: "France",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+  },
+  {
+    id: 3,
+    title: "Italy",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://images.huffingtonpost.com/2015-12-15-1450212697-8869502-156689b81c3c9e01555a4ddc1_positanoamalficoastitalycralamy.jpg",
+  },
+  {
+    id: 4,
+    title: "Norway",
+    price: "$59.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://www.worldatlas.com/r/w1200/upload/04/92/d1/shutterstock-382716112.jpg",
+  },
+  {
+    id: 5,
+    title: "Spain",
+    price: "$19.99",
+    category: "Nintendo Switch",
+    linkImg:
+      "https://www.divergenttravelers.com/wp-content/uploads/Best-Places-to-Visit-in-Spain.jpg",
+  },
+  {
+    id: 6,
+    title: "Turkey",
+    price: "$29.99",
+    category: "Nintendo Switch",
+    linkImg: "https://i.ytimg.com/vi/sKgwpCrdGiU/maxresdefault.jpg",
+  },
+];
